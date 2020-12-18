6cfae11b6076280f0c200829afd411fd56c7e3bb
C.T. Colwyn <ct@colwyn.net>
2020-12-17 22:05:53 -0600

    fixed bug in deploy script where argument was not captured

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 # Build site; if successful, push to GitHub.
 # (NOTE: Script is simplistic and should be run with project root as working directory.)
-hugo && git add . && git commit -m '$1' && git push && \
+hugo && git add . && git commit -m "$1" && git push && \
 hugo deploy
 
 echo '\nBad links listed below, if any. If not, done!'
