173abd351a69f2ec6bdf612eb3b195429f53b2ce
C.T. Colwyn <ct@colwyn.net>
2020-12-17 18:34:34 -0600

    $1

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,7 @@
 # Build site; if successful, push to GitHub.
 # (NOTE: Script is simplistic and should be run with project root as working directory.)
 hugo && git add . && git commit -m '$1' && git push && \
-    \ # TODO: At some point, implement link checker at this stage
 hugo deploy
+
+echo '\n\n'
+linkchecker 'https://colwyn.net'
